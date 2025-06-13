5d172c0a0bb43346eee071c8b05fdf10875465f3
mohittsharma07 <mohittt247@gmail.com>
2025-06-13 16:31:24 +0530

    Added while loop program to print numbers from 1 to 50

---
+++ whileloop.c
@@ -0,0 +1,14 @@
+// program to print numbers from 1 to 50.
+#include<stdio.h>
+
+int main() {
+    int i = 1;
+    printf("Numbers from 1 to 50 \n");
+    while (i <= 50) {
+        printf("%d \n",i);
+        i++; // increment i
+    }
+    
+    printf(" ");
+    return 0;
+}
