c34dfe718366318771a48c2aedcb2eeecb048951
mohittsharma07 <mohittt247@gmail.com>
2025-06-17 13:10:14 +0530

    Tables program

---
+++ table.c
@@ -0,0 +1,16 @@
+// Print table of a number.
+
+#include<stdio.h>
+int main() {
+    int num, i;
+    printf("Enter a number6: ");
+    scanf("%d", &num);
+
+    printf("Multiplication Table of %d:\n", num);
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
+
